298fb30bf546e01f652cd0886955a8d869cd33c6
xhan97 <xhan97@users.noreply.github.com>
2024-06-18 03:36:12 +0000

    [ci skip] add ik_inne 41d3b8671d61aa719f3f4143fa86e1218ab05f0e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev8+g9e0aa3d.d20240615',
+    VERSION: '0.1.dev10+g41d3b86.d20240618',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
