299538503ca6c9cfd9d27adc16b55474fcc3edd7
xhan97 <xhan@tulip.academy>
2024-06-20 01:13:27 +0800

    isokhc

--- isoml/cluster/_isokhc.py
+++ isoml/cluster/_isokhc.py
@@ -2,19 +2,6 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# Copyright 2022 Xin Han
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import numpy as np
 from scipy.cluster.hierarchy import linkage
@@ -57,7 +44,7 @@ class IsoKHC(BaseEstimator):
     ----------
     .. [1] Xin Han, Ye Zhu, Kai Ming Ting, and Gang Li,
            "The Impact of Isolation Kernel on Agglomerative Hierarchical Clustering Algorithms",
-           arXiv e-prints, 2020.
+           Pattern Recognition, 2023, 139: 109517.
 
     Examples
     --------
