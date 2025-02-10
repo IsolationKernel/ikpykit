af72f11c5b9456a3d7c17c63bc2b4a4757b9677a
Xin Han <xhan@tulip.academy>
2025-02-10 10:38:08 +0800

    docs readme

--- pyike/_version.py
+++ pyike/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev107+gd6a1fd8.d20250114'
-__version_tuple__ = version_tuple = (0, 1, 'dev107', 'gd6a1fd8.d20250114')
+__version__ = version = '0.1.dev109+gead9b78.d20250210'
+__version_tuple__ = version_tuple = (0, 1, 'dev109', 'gead9b78.d20250210')
