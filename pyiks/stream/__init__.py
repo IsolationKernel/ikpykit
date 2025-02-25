4cb4baae5c94e2b3709c7c5860915e1e91d47408
Xin Han <xhan197@outlook.com>
2025-02-25 14:55:08 +1100

    icid

--- pyiks/stream/__init__.py
+++ pyiks/stream/__init__.py
@@ -8,8 +8,10 @@ You should have received a copy of the license along with this
 work. If not, see <https://creativecommons.org/licenses/by-nc-nd/4.0/>.
 """
 
-from .anomaly._ikgad import IKGAD
+from .changedetect._icid import ICID
+from .cluster._streaKHC import StreaKHC
 
 __all__ = [
-    "IKGAD",
+    "ICID",
+    "StreaKHC",
 ]
