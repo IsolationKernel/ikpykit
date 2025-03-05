d3607ef1341a017804b2aa825ccaaf38f331fc8d
Xin Han <xhan@tulip.academy>
2025-03-05 18:20:00 +1100

    fix format

--- pyikt/stream/cluster/utils/serialize_trees.py
+++ pyikt/stream/cluster/utils/serialize_trees.py
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 import math
 
 import numpy as np
@@ -22,13 +23,13 @@ from numba import jit
 def _fast_dot(x, y):
     """Compute the dot product of x and y using numba.
 
-      Args:
-      x - a numpy vector (or list).
-      y - a numpy vector (or list).
+    Args:
+    x - a numpy vector (or list).
+    y - a numpy vector (or list).
 
-      Returns:
-      x_T.y
-      """
+    Returns:
+    x_T.y
+    """
 
     return np.dot(x, y)
 
@@ -75,20 +76,30 @@ except:
 
 
 def serliaze_tree_to_file_with_point_ids(root, fn):
-    with open(fn, 'w') as fout:
+    with open(fn, "w") as fout:
         queue = Queue()
         queue.put(root)
         while not queue.empty():
             curr_node = queue.get()
-            curr_node_id = curr_node.pts[0][1] if curr_node.is_leaf(
-            ) else curr_node.id
+            curr_node_id = curr_node.pts[0][1] if curr_node.is_leaf() else curr_node.id
             sibling_node_id = "None"
             if curr_node.parent:
-                sibling_node_id = curr_node.siblings()[0].pts[0][1] if curr_node.siblings()[
-                    0].is_leaf() else curr_node.siblings()[0].id
+                sibling_node_id = (
+                    curr_node.siblings()[0].pts[0][1]
+                    if curr_node.siblings()[0].is_leaf()
+                    else curr_node.siblings()[0].id
+                )
             dis = getDistance(curr_node) if curr_node.parent else "None"
-            fout.write("%s\t%s\t%s\t%s\t%s\n" % (curr_node_id, sibling_node_id,
-                       curr_node.parent.id if curr_node.parent else "None", dis, curr_node.pts[0][0] if curr_node.is_leaf() else "None"))
+            fout.write(
+                "%s\t%s\t%s\t%s\t%s\n"
+                % (
+                    curr_node_id,
+                    sibling_node_id,
+                    curr_node.parent.id if curr_node.parent else "None",
+                    dis,
+                    curr_node.pts[0][0] if curr_node.is_leaf() else "None",
+                )
+            )
             for c in curr_node.children:
                 queue.put(c)
 
@@ -98,34 +109,59 @@ def getDistance(curr_node):
     curr_point_count = curr_node.point_counter
     sibling_point_count = sibling.point_counter
 
-    distance = 2*(200 - (_fast_dot(sibling.ikv, curr_node.ikv) /
-                  (curr_point_count*sibling_point_count)))
+    distance = 2 * (
+        200
+        - (
+            _fast_dot(sibling.ikv, curr_node.ikv)
+            / (curr_point_count * sibling_point_count)
+        )
+    )
 
     return distance
 
 
 def serliaze_tree_to_file(root, fn):
-    with open(fn, 'w') as fout:
+    with open(fn, "w") as fout:
         queue = Queue()
         queue.put(root)
         while not queue.empty():
             curr_node = queue.get()
-            fout.write("%s\t%s\t%s\t%s\n" % (curr_node.id, curr_node.parent.id if curr_node.parent else "None",
-                       curr_node.pts[0][1] if curr_node.is_leaf() else "None", len(curr_node.pts)))
+            fout.write(
+                "%s\t%s\t%s\t%s\n"
+                % (
+                    curr_node.id,
+                    curr_node.parent.id if curr_node.parent else "None",
+                    curr_node.pts[0][1] if curr_node.is_leaf() else "None",
+                    len(curr_node.pts),
+                )
+            )
             for c in curr_node.children:
                 queue.put(c)
 
 
 def serliaze_collapsed_tree_to_file_with_point_ids(root, fn):
-    with open(fn, 'w') as fout:
+    with open(fn, "w") as fout:
         queue = Queue()
         queue.put(root)
         while not queue.empty():
             curr_node = queue.get()
-            curr_node_id = curr_node.pts[0][2] if curr_node.is_leaf(
-            ) and not curr_node.is_collapsed else curr_node.id
-            fout.write("%s\t%s\t%s\n" % (curr_node_id, curr_node.parent.id if curr_node.parent else "None",
-                       curr_node.pts[0][1] if curr_node.is_leaf() and not curr_node.is_collapsed else "None"))
+            curr_node_id = (
+                curr_node.pts[0][2]
+                if curr_node.is_leaf() and not curr_node.is_collapsed
+                else curr_node.id
+            )
+            fout.write(
+                "%s\t%s\t%s\n"
+                % (
+                    curr_node_id,
+                    curr_node.parent.id if curr_node.parent else "None",
+                    (
+                        curr_node.pts[0][1]
+                        if curr_node.is_leaf() and not curr_node.is_collapsed
+                        else "None"
+                    ),
+                )
+            )
             for c in curr_node.children:
                 queue.put(c)
             if curr_node.collapsed_leaves is not None:
